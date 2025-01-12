0c63d7d66f7ee421c3ad8476fca95ef6f16e23c6
Caraffa-git <caraffa445@gmail.com>
2025-01-12 23:29:00 +0100

    lcsc quick fix

--- backend/lcsc.py
+++ backend/lcsc.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 import re
+import json
 import requests
 from typing import Type
 from parsel import Selector
@@ -34,30 +35,27 @@ class LCSC():
             if response.status_code == 200:
                 #print("Query successful!")
                 sel = Selector(response.text)
-                # gets the content of the name field
-                fields["name"] = re.sub(' +', ' ', self.utils.cleanhtml(sel.xpath("/html/body/div/div/div/div/main/div/div/div[2]/div/div/div[1]/div[1]/div[2]/table/tbody/tr[2]/td[2]").get()).strip())
+
                 try:
-                    # gets the content of the description field
-                    fields["description"] = re.sub(' +', ' ', self.utils.cleanhtml(sel.xpath("/html/body/div/div/div/div[1]/main/div/div/div[2]/div/div/div[1]/div[1]/div[2]/table/tbody/tr[9]/td[2]").get()).strip())
-                except:
-                    # I don't remember what this is for, I think there are some parts with a weird descriptions or something
-                    #fields["description"] = re.sub(' +', ' ', self.utils.cleanhtml(sel.xpath("/html/body/div/div/div/div[1]/main/div/div/div[2]/div/div/div[1]/div[1]/div[2]/table/tbody/tr[7]/td[2]").get()).strip())
-                    print("FIXME - weird description")
-                # gets the name of the part category from the navigation strip on top of the page
-                fields["template_description"] = re.sub(' +', ' ', self.utils.cleanhtml(sel.xpath("/html/body/div/div/div/div[1]/main/div/div/div[1]/div/div/ul/li[7]/a").get()).strip())
-                # gets the package field
-                package = re.sub(' +', ' ', self.utils.cleanhtml(sel.xpath("/html/body/div/div/div/div[1]/main/div/div/div[2]/div/div/div[1]/div[1]/div[2]/table/tbody/tr[4]/td[2]").get()).strip())
-                # gets the image link
-                fields["remote_image"] = re.sub(' +', ' ', sel.css('div.v-card:nth-child(1) > img:nth-child(1)').attrib['src'])
-                # gets the link to the part
-                fields["link"] = response.url
-                # check if part is discontinued beofore getting the price
-                try:
-                    # gets the price field of the part, if the price does not exist then the part is discontinued
-                    fields["unit_price"] = re.sub(' +', ' ', self.utils.cleanNumeric(self.utils.cleanhtml(sel.xpath("/html/body/div/div/div/div[1]/main/div/div/div[2]/div/div/div[2]/div[1]/div[4]/table/tbody/tr[1]/td[2]/span").get())).strip())
-                except:
-                    print("Part is discontinued!")
-                    fields["unit_price"] = "0"
+                # gets the content of script tag containing full info
+                    data = json.loads(sel.xpath("/html/head/script[4]/text()").get())
+
+                    fields["name"] = data["name"]
+                    fields["description"]  = data["sku"]
+                    fields["template_description"] = data["category"].split("/")[1]
+                    fields["remote_image"] = data["image"]
+                    fields["link"] = response.url
+                    fields["unit_price"] = data["offers"]["price"]
+                    package = re.sub(' +', ' ', self.utils.cleanhtml(sel.xpath("/html/body/div/div/div/div[1]/main/div/div/div/div/div[1]/div[1]/div[2]/table/tbody/tr[4]/td[2]/div/span").get()).strip())
+
+                    if fields["unit_price"] == "0.00":
+                        print("Part is discontinued!")
+
+                except Exception as e:
+                    print(e)
+                    print("No data found")
+
+
         except Exception as e:
             print(e)
             print("Invalid part number!")
